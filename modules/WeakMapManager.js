01ff2e448ae46c823ab9f2c49bb670ada99b6765
jwMoon <jwmoon@nivuskorea.com>
2025-04-11 10:32:24 +0900

    weakmapManager 사용법 예제 작성 및 폴더명 파일명 변경

--- modules/WeakMapManager.js
+++ modules/WeakMapManager.js
@@ -19,18 +19,28 @@ class WeakMapManager {
     // id로 값을 가져오는 메서드
     getKey(id) {
         let keyObj = this.keys.get(id);
+        return keyObj;
+    }
 
+    getData(id) {
+        const keyObj = this.keys.get(id); // Map에서 id에 해당하는 키 객체를 가져옴
+    
         if (keyObj) {
-            return keyObj;
+            return this.weakMap.get(keyObj); // WeakMap에서 해당 키 객체로 값을 가져옴
         } else {
-            console.log("Key not found");
-            return this.setKey(id);  // 키가 없을 경우 키 생성 반환
+            console.log(`Key (${id}) not found`);
+            return null; // 키가 없을 경우 null 반환
         }
     }
 
+
     // id로 값을 추가하는 메서드
     add(id, value) {
-        const keyObj = this.getKey(id);  // setKey 호출해서 id로 키를 얻음
+        let keyObj = this.getKey(id);  // setKey 호출해서 id로 키를 얻음
+        if (!keyObj || keyObj == null) {  // 키가 없을 경우
+            console.log(`Key (${id}) not found`);
+            keyObj = this.setKey(id);  // 키가 없을 경우 키 생성 반환
+        } 
         this.weakMap.set(keyObj, value);  // WeakMap에 값을 추가
     }
 
@@ -40,7 +50,7 @@ class WeakMapManager {
         if (keyObj) {
             keyObj.description = newDesc;  // 기존 객체의 desc 값만 수정
         } else {
-            console.log("Key not found");
+            console.log(`Key (${id}) not found`);
         }
     }
 
